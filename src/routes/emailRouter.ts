5b48552c1c59130105d1c2ff5384e1fccb8746d5
Filip Nedeljković <nedeljkovic.filip.dev@gmail.com>
2024-08-04 18:07:07 +0200

    Deleted test

--- src/routes/emailRouter.ts
+++ src/routes/emailRouter.ts
@@ -5,6 +5,5 @@ const emailRouter = Router();
 const emailService = new EmailService();
 
 emailRouter.post('/', (req, res) => emailService.sendEmail(req, res));
-emailRouter.get('/', (req, res) => emailService.getEmail(req, res));
 
 export { emailRouter };
